3e335d19b30213ac33a7ced6838d3a2856770cb8
NimaTahmidi <nimafaghihi99@gmail.com>
2024-11-06 13:41:13 +0100

    Initial commit

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+
+    @Test
+    public void simpleTests(){
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void simpleTest1(){
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void simpleTest2(){
+        Assert.assertTrue(true);
+    }
+
+
+}
